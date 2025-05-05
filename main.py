ff79e4b5fb482f600ec478b04846a3e966d8d4b7
Alex-Beh <chunjye088@Gmail.com>
2025-05-05 19:14:37 +0800

    setup for render deploy

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from app import create_app, db
 app = create_app()
 CORS(app)
 
+with app.app_context():
+    db.create_all()  # Create tables if they don't exist
+
 # Run the server
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Create tables if they don't exist
     app.run(host="0.0.0.0", port=5000, debug=True)
