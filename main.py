0c00fe1e386d831bf6252650a1b4c04b95e2aa1f
Alex-Beh <chunjye088@Gmail.com>
2025-05-05 19:47:55 +0800

    update gunicorn command

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ CORS(app)
 with app.app_context():
     db.create_all()  # Create tables if they don't exist
 
-# Run the server
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# # Run the server
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=True)
